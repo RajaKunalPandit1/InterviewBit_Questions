3a55c02a01f3da6ce60bcfea97d97a86aef0a420
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2021-05-22 14:38:57 +0530

    Create Sample Question .cpp
    Given two integers A and B. Find the sum of these integers as a modulo of 10^7.
    
    Problem Constraints
    0 <= A, B <= 109
    Output Status:
    Correct Answer. You got 0/0 points!

---
+++ Sample Question .cpp
@@ -0,0 +1,10 @@
+int Solution::solve(int A, int B) {
+    return ((A+B) % 10000000);
+}
+// Another Solution 
+
+int Solution::solve(int A, int B) {
+    
+    int c=(A+B)%10000000;
+    return c;
+}
